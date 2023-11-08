a5530cb72a7065acff1b8b7880b6b7dd557fe418
yyyannnn <150150320+yyyannnn@users.noreply.github.com>
2023-11-08 09:08:57 -0500

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student{
+  public String firstName;
+  public String lastName;
+  public static totalStudents = 0;
+
+  public Student(first,last){
+    firstName = first;
+    lastName = last;
+    totalStudents++;
+  } 
+}
+  
