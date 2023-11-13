b03e589fad68d090b1420af125638c9e149063af
yyyannnn <349168872@gapps.yrdsb.ca>
2023-11-13 08:44:47 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,11 +1,13 @@
 public class Student{
   public String firstName;
   public String lastName;
+  public int grade;
   public static totalStudents = 0;
 
-  public Student(first,last){
+  public Student(first,last, theGrade){
     firstName = first;
     lastName = last;
+    grade = theGrade;
     totalStudents++;
   } 
 }
