a19a471cf733382f30a049bd9de27cfd6062619c
yyyannnn <150150320+yyyannnn@users.noreply.github.com>
2023-11-13 09:05:21 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,12 +2,14 @@ public class Student{
   public String firstName;
   public String lastName;
   public int grade;
+  public int mark;
   public static totalStudents = 0;
 
-  public Student(first,last, theGrade){
+  public Student(first,last, theGrade, theMark){
     firstName = first;
     lastName = last;
     grade = theGrade;
+    mark = theMark;
     totalStudents++;
   } 
 }
